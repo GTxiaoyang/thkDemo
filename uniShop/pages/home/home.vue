6f30f0ce49c2b6339f89ae11f989e54c63479426
GTxiaoyang <1725827084@qq.com>
2022-02-21 22:49:09 +0800

    添加

---
+++ uniShop/pages/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				swiperData:[]
+			};
+		},
+		onLoad() {
+			this.getSwiperdata()
+		},
+		methods:{
+			async getSwiperdata(){
+				let {data:res}=await uni.$http.get('api/public/v1/home/swiperdata')
+				this.swiperData=res.message
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
