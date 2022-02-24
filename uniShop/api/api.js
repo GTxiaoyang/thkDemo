37c75cc8a31d3b6b1c2f6fe51cd437ffd800b48d
GTxiaoyang <54778780+GTxiaoyang@users.noreply.github.com>
2022-02-25 00:33:22 +0800

    nav

--- uniShop/api/api.js
+++ uniShop/api/api.js
@@ -1,2 +1,4 @@
 //轮播图
 export const getSwiperdataFn = ()=> uni.$http.get('api/public/v1/home/swiperdata')
+// 导航
+export const getNavListFn = ()=> uni.$http.get('api/public/v1/home/catitems')
--- uniShop/pages/home/home.vue
+++ uniShop/pages/home/home.vue
@@ -3,29 +3,49 @@
 		<!-- 轮播图 -->
 		<swiper :indicator-dots="true" indicator-active-color="#d81e06" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
 			<swiper-item v-for="(item,index) in swiperData" :key="index">
-				<navigator :url="'/subpkg/goods_detail/goods_detail?goods_detail='+ item.goods_id" class="swiper-item">
-					<img :src="item.image_src">
+				<navigator :url="'/subpkg/goods_detail/goods_detail?goods_detail='+ item.goods_id" >
+					<img class="lbtImage" :src="item.image_src">
 				</navigator>
 			</swiper-item>
 		</swiper>
+		
+		<!-- 导航区 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item,i) in navList" :key="i">
+				<img :src="item.image_src" alt="" class="nav-img">
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	import {getSwiperdataFn} from '../../api/api.js'
+	import {getSwiperdataFn,getNavListFn} from '../../api/api.js'
 	export default {
 		data() {
 			return {
-				swiperData:[]
+				// 轮播图
+				swiperData:[],
+				// 导航
+				navList:[]
+				
 			};
 		},
 		onLoad() {
+			// 获取轮播图
 			this.getSwiperdata()
+			//获取导航
+			this.getNavList()
 		},
 		methods:{
+			// 获取轮播图
 			async getSwiperdata(){
 				let {data:res}=await getSwiperdataFn()
 				this.swiperData=res.message
+			},
+			//获取导航信息
+			async getNavList(){
+				let {data:res}=await getNavListFn()
+				this.navList=res.message
 			}
 		}
 	}
@@ -34,9 +54,20 @@
 <style lang="scss">
 swiper{
 	height: 330rpx;
-	.swiper-item,.swiper-item image{
+	.swiper-item,.lbtImage{
 		width: 100%;
 		height: 100%;
+	}
+}
+.nav-list{
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	margin: 15px 0;
+	
+	.nav-img{
+		width:128rpx;
+		height: 140rpx;
 	}
 }
 </style>
